eddc1a73b127341b31719ca8d0bb5883dac4994c
wussy1 <wisssalah888@gmail.com>
2022-06-10 16:21:33 +0100

    kk

---
+++ back/routes/Panier_routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { AddItem, DeleteItem, getpanier_by_user, MinusItem, PlusItem } from "../controllers/Panier_controller.js";
+
+
+const router = express.Router();
+
+router.get("/by-user-id/:id",getpanier_by_user)
+router.post("/plus",PlusItem)
+router.post("/minus",MinusItem)
+router.delete("/delete", DeleteItem);
+router.post("/add", AddItem);
+
+
+
+export default router;
